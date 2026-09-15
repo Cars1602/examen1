c7108489210a3637c2f47980d1b48cb832f8d806
Cars1602 <cars1602@gmail.com>
2026-09-15 12:50:08 -0400

    imagenes

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const sequelize = require('./config/database');
+const app = express();
+const Producto = require('./models/Producto');//Importamos el modelo Producto
+const productoRoutes = require('./routes/productoRoutes');
+app.use(express.json());
+app.use('/api', productoRoutes);
+
+app.get('/', (req, res) => {
+    res.send('¡Bienvenido a la API de Productos!');
+});
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log(
+            'Conexión a la base de datos establecida correctamente.');
+    })
+    .catch((error) => {
+        console.error(
+            'Error al conectar a la base de datos:', error);
+    });
+app.listen(3000, () => {
+    console.log('Servidor ejecutándose en http://localhost:3000');
+});
+sequelize.sync()
+    .then(() => {
+        console.log('Base de datos sincronizada correctamente.');
+    })
+    .catch((error) => {
+        console.error('Error al sincronizar la base de datos:', error);
+    });
